77e9ea0379c0449eea0ac5dff816baf46880b5f8
Etanoke <etanoke@users.noreply.github.com>
2016-09-17 15:23:07 +0900

    optimize command of installation for Python3

--- pytnon3_install.sh
+++ pytnon3_install.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 SCRIPT_NAME=`basename ${0}`
-DEFAULT_PYTHON3_VERSION="3.5.1"
 
-while getopts 3v:mh OPT
+while getopts 3meh OPT
 do
-	case $OPT in
-		3 ) PYTHON3_FLAG="TRUE";;
-        v ) VERSION=$OPTARG;;
-		m ) MATLAB_FLAG="TRUE";;
-		h ) HELP_FLAG="TRUE";;
-	esac
+    case $OPT in
+        3 ) PYTHON3_FLAG="TRUE";;
+        m ) MATLAB_FLAG="TRUE";;
+        e ) PYENV="TRUE";;
+        h ) HELP_FLAG="TRUE";;
+    esac
 done
 
+
 if [ $HELP_FLAG ] || [ $# = 0 ]; then
-	cat << __EOD__
-Usage: $SCRIPT_NAME [-3] [-v ver] [-m]" 
-  -3        Install Python3
-  -v ver    Python3 version(default 3.5.1)
+    cat << __EOD__
+Usage: $SCRIPT_NAME [-3emh] [-v ver]" 
+  -3        Install required packages for Python3
   -m        Install required packages for matplotlib
+  -e        Install pyenv($HOME/.pyenv)
   -h        Show this help message
 __EOD__
-	exit
+    exit
 fi
 
 if [ $PYTHON3_FLAG ]; then
-	if [ ! "$VERSION" ]; then
-		VERSION=$DEFAULT_PYTHON3_VERSION
-	fi
-	yum install gcc-c++
-	yum install openssl-devel
-	yum install readline-devel
-	git clone https://github.com/yyuu/pyenv /usr/local/bin/pyenv
-	echo 'export PYENV_ROOT="/usr/local/bin/pyenv"' >> ~/.bashrc
-<< __OTHER_WAY__
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-__OTHER_WAY__
-	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-	soruce ~/.bashrc
-	pyenv install $VERSION
-	yum install python-setuptools
-	easy_install virtualenv
+    yum install gcc-c++
+    yum install openssl-devel
+    yum install readline-devel
+    yum install python-setuptools
+    easy_install virtualenv
 fi
 
 if [ $MATLAB_FLAG ]; then
-	yum install freetype-devel
-	yum install libpng-devel
+    yum install freetype-devel
+    yum install libpng-devel
+fi
+
+if [ $PYENV ]; then
+    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+<< __INSTALL_TO_GLOBAL__
+    git clone https://github.com/yyuu/pyenv /usr/local/bin/pyenv
+    echo 'export PYENV_ROOT="/usr/local/bin/pyenv"' >> /etc/bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bashrc
+__INSTALL_TO_GLOBAL__
+
+	cat <<AFTER_MESSAGE
+
+Installation of pyenv is complete!
+To install Python3:
+$ source ~/.bashrc
+$ pyenv install [version] 
+AFTER_MESSAGE
 fi
