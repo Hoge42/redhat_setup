a8aff452a1ad0507e3cfe2871a3a64688f692ad5
Etanoke <etanoke@users.noreply.github.com>
2016-12-21 09:26:16 +0900

    gitの設定スクリプトを追加

---
+++ git_setup.sh
@@ -0,0 +1,8 @@
+git config --global user.name "hoge"
+git config --global user.email hoge@example.com
+git config --global color.ui true
+git config --global core.editor vim
+git config --global core.pager less
+git config --global color.ui true
+git config --global alias.alllog 'log --all --oneline --decorate --graph'
+echo 'source /etc/bash_completion.d/git' >> $HOME/.bashrc
