6f1e507f163b1fd7117b9a7798115499b322b35a
Etanoke <etanoke@users.noreply.github.com>
2016-12-26 17:39:57 +0900

    Update git_setup.sh
    重複している設定を削除

--- git_setup.sh
+++ git_setup.sh
@@ -3,6 +3,5 @@ git config --global user.email hoge@example.com
 git config --global color.ui true
 git config --global core.editor vim
 git config --global core.pager less
-git config --global color.ui true
 git config --global alias.alllog 'log --all --oneline --decorate --graph'
 echo 'source /etc/bash_completion.d/git' >> $HOME/.bashrc
