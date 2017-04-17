0bb989a42006969f25bdb4a54c72758479e6b6a9
Etanoke <etanoke@users.noreply.github.com>
2017-04-17 10:12:38 +0900

    git設定スクリプト更新
    * 同名ブランチのupstreamが設定されている場合のみ`git push`可能
    * git logのでコミット者の名前を表示

--- git_setup.sh
+++ git_setup.sh
@@ -3,5 +3,6 @@ git config --global user.email hoge@example.com
 git config --global color.ui true
 git config --global core.editor vim
 git config --global core.pager less
-git config --global alias.alllog 'log --all --oneline --decorate --graph'
+git config --global push.default simple
+git config --global alias.alllog 'log --all --graph --date=short --format="%C(yellow)%h%C(reset) %C(magenta)[%ad]%C(reset)%C(auto)%d%C(reset) %s %C(cyan)@%an%C(reset)"'
 echo 'source /etc/bash_completion.d/git' >> $HOME/.bashrc
